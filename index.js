ab918e7652527af6cedff001f34c1b052deff3c4
Austin Meyer <frizzkitten@gmail.com>
2017-12-15 15:56:00 -0600

    put settings in its own function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const credentials = require('./credentials');
 const difflib = require('difflib');
+const twilio = require('twilio');
 
 const app = express();
 
@@ -41,56 +42,12 @@ app.post('/sms', function(request, response) {
         outMessage = msg;
       }
 
+      // if user is in the process of changing settings, change them
       else if (user.changingSettings) {
-          if (user.settingToChange) {
-              let updated = { settingToChange: undefined, changingSettings: undefined }
-              if (user.settingToChange == "name") {
-                  updated.name = inMessage;
-                  outMessage = "Your name has been changed name to " + inMessage;
-              } else if (user.settingToChange == "default pickup location") {
-                  let location = parseMessage(inMessage, "getLocation");
-                  console.log(location);
-                  if (!location) {
-                      outMessage = "Sorry, that isn't a location I recognize. Not changing settings.";
-                  } else {
-                      updated.pickupLocation = location;
-                      outMessage = "Your default pickup location is now " + location + ".";
-                  }
-              }
-
-              const update = {'$set': updated};
-              Users.findOneAndUpdate(query, update, options, function (err, user) {
-                  if (err) { console.log(err); }
-              });
-          }
-
-          // user is entering which setting they want to change
-          else {
-              let settingToChange = parseMessage(inMessage, "settingToChange");
-              // user didn't enter 'name' or 'default pickup location', exit settings mode
-              if (!settingToChange) {
-                  outMessage = "I didn't understand that, sorry! Not changing any settings.";
-                  const update = {'$set': { changingSettings:false } };
-                  Users.findOneAndUpdate(query, update, options, function (err, user) {
-                      if (err) { console.log(err); }
-                  });
-              }
-              // user correctly entered setting to change, tell DB we will change that setting
-              else {
-                  //const locations = ""
-                  outMessage = "What would you like to change your " + settingToChange + " to?";
-                  if (settingToChange == "default pickup location") {
-                      outMessage = outMessage + " (Hub, McDonald's, or Porter Boathouse)"
-                  }
-                  const update = { settingToChange: settingToChange };
-                  Users.findOneAndUpdate(query, update, options, function (err, user) {
-                      if (err) { console.log(err); }
-                  });
-              }
-          }
+          outMessage = changeSettings(user, inMessage, query, options);
       }
 
-      //this means the person has an account
+      // this means the person has an account and is not changing settings
       else {
         // parse to figure out which command the person wants to perform
         let choice = parseMessage(inMessage, false);
@@ -134,7 +91,6 @@ app.post('/sms', function(request, response) {
             outMessage = "You are no longer signed up for practice.";
             break;
           case "settings":
-            //TODO settings function
             outMessage = "Would you like to change your name or default pickup location?";
             const update = { changingSettings: true };
             Users.findOneAndUpdate(query, update, options, function (err, user) {
@@ -146,7 +102,6 @@ app.post('/sms', function(request, response) {
         }
       }
 
-      let twilio = require('twilio');
       let twiml = new twilio.twiml.MessagingResponse();
       twiml.message(outMessage);
       response.writeHead(200, {'Content-Type': 'text/xml'});
@@ -270,6 +225,60 @@ function checkIfNewbie(inText, number, callback) {
       callback(newbie, outMessage, user);
   })
 }
+
+// either set the setting to change or change the actual setting, then return
+// an outMessage
+function changeSettings(user, inMessage, query, options) {
+    if (user.settingToChange) {
+        let updated = { settingToChange: undefined, changingSettings: undefined }
+        if (user.settingToChange == "name") {
+            updated.name = inMessage;
+            outMessage = "Your name has been changed name to " + inMessage;
+        } else if (user.settingToChange == "default pickup location") {
+            let location = parseMessage(inMessage, "getLocation");
+            console.log(location);
+            if (!location) {
+                outMessage = "Sorry, that isn't a location I recognize. Not changing settings.";
+            } else {
+                updated.pickupLocation = location;
+                outMessage = "Your default pickup location is now " + location + ".";
+            }
+        }
+
+        const update = {'$set': updated};
+        Users.findOneAndUpdate(query, update, options, function (err, user) {
+            if (err) { console.log(err); }
+        });
+    }
+
+    // user is entering which setting they want to change
+    else {
+        let settingToChange = parseMessage(inMessage, "settingToChange");
+        // user didn't enter 'name' or 'default pickup location', exit settings mode
+        if (!settingToChange) {
+            outMessage = "I didn't understand that, sorry! Not changing any settings.";
+            const update = {'$set': { changingSettings:false } };
+            Users.findOneAndUpdate(query, update, options, function (err, user) {
+                if (err) { console.log(err); }
+            });
+        }
+        // user correctly entered setting to change, tell DB we will change that setting
+        else {
+            //const locations = ""
+            outMessage = "What would you like to change your " + settingToChange + " to?";
+            if (settingToChange == "default pickup location") {
+                outMessage = outMessage + " (Hub, McDonald's, or Porter Boathouse)"
+            }
+            const update = { settingToChange: settingToChange };
+            Users.findOneAndUpdate(query, update, options, function (err, user) {
+                if (err) { console.log(err); }
+            });
+        }
+    }
+
+    return outMessage;
+}
+
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
